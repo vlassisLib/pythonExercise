becbb49bdfc98c0845fe850fbcb71125732bd36d
Vlassis Liberopoulos <soukos98@hotmail.com>
2020-05-22 23:41:23 +0300

    Class.py

---
+++ Class.py
@@ -0,0 +1,31 @@
+food_menu = ["Pizza","Burger","Tortilla"]
+drink_menu = ["Coca-cola","Water"]
+class Order:
+    def __init__(self,menu):
+        self.menu = menu
+   
+    def food_order(self):
+        print(self.menu)
+        food_selection = input("Select your food: ")
+        if food_selection in self.menu:
+            print("Your ordered {}".format(food_selection))
+    
+    def drink_order(self):
+        print(self.menu)
+        drink_selection = input("Select your drink: ")
+        if drink_selection in self.menu:
+            print("You ordered {}".format(drink_selection))
+
+
+def order():
+    food = Order(food_menu)
+    food.food_order()
+    drink = Order(drink_menu)
+    drink.drink_order()
+
+order()
+
+
+
+
+
---
+++ ConnectDb.py
@@ -0,0 +1,5 @@
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+
+from datetime import datetime
